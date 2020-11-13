eb7ecaf2e41a5250b40490e684ea50096f373ea7
Eric <escott@structures.com>
2020-11-13 16:18:57 -0700

    printing out page form to log

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -167,6 +167,9 @@ def lambda_handler(event, context):
 
     printresponsetos3(doc)
 
+    for page in doc.pages:
+        for form1 in page.form:
+            print(form1)
 """
     all_values = []
 
