8fd766b271d2e977cbc061f7623b2b6792ba3936
Eric Scott <escott@archer.local>
2020-11-17 16:18:18 -0700

    add document info print to log specific name - bug fix

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -188,7 +188,7 @@ def lambda_handler(event, context):
     print('printing doc location')
     print(pdfTextExtractionDocLoc)
 
-    docname = json.loads(str(pdfTextExtractionDocLoc))['S3ObjectName']
+    docname = pdfTextExtractionDocLoc['S3ObjectName']
     print('document name is: '+docname)
     if(pdfTextExtractionStatus == 'SUCCEEDED'):
         response = getJobResults(pdfTextExtractionJobId)
