a035f9bb1de3e118d7d06588520fd46fb0dbce7f
Eric Scott <escott@archer.local>
2020-11-25 10:09:50 -0700

    bug fix

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -202,7 +202,7 @@ def lambda_handler(event, context):
     for page in doc.pages:
         for field in page.form.fields:
             print(f'key found in form:{field.key}: with value :{field.value}:')
-            all_keys.append(field.key)
+            all_keys.append(str(field.key))
             if str(field.key) == 'Phone':
                 phone = field.value
                 print('the phone number is: ',field.value)
