22cd9bf869b759b539718e155ccfb616e96dcf2d
Eric Scott <escott@archer.local>
2020-12-03 10:15:04 -0700

    bug fix

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -285,7 +285,7 @@ def lambda_handler(event, context):
             print(f"i found {str(len(lFields))} field objects")
             if(len(lFields)>0):
                 correctField = GetFromTheTop(lFields,0)
-            else 
+            else:
                 correctField = lFields[0]
 
             print(f'--- ther correctField is {correctField.value}')
