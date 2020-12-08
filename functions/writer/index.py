e52824ced7193c59f1fe2d35f861396cb25da83c
Eric Scott <escott@archer.local>
2020-12-08 10:33:50 -0700

    added YES to map

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -143,7 +143,8 @@ csv_2_ocr_map_enroll = {
 'State':{'ocr_key':'State', 'PageNo': 2, 'TopPos': 1},
 'Zip Code':{'ocr_key':'Zip', 'PageNo': 2, 'TopPos': 1},
 'Address 1':{'ocr_key':'Street/P.O. B', 'PageNo': 2, 'TopPos': 1},
-'Claimant Date of Birth':{'ocr_key':'(Month/Day/Year)', 'PageNo': 3, 'TopPos': 1}
+'Claimant Date of Birth':{'ocr_key':'(Month/Day/Year)', 'PageNo': 2, 'TopPos': 1},
+'YES':{'ocr_key':'Current Citizenship Status','PageNo': 2, 'TopPos': 1}
 }
 
 csv_2_ocr_map_relfull = {
@@ -154,6 +155,7 @@ csv_2_ocr_map_relfull = {
 'Zip Code':{'ocr_key':'Zip', 'PageNo': 3, 'TopPos': 1},
 'Address 1':{'ocr_key':'Street/P.O. B', 'PageNo': 3, 'TopPos': 1},
 'Claimant Date of Birth':{'ocr_key':'(Month/Day/Year)', 'PageNo': 3, 'TopPos': 1}
+'YES':{'ocr_key':'Current Citizenship Status','PageNo': 3, 'TopPos': 1}
 }
 
 #csv_2_ocr_map = csv_2_ocr_map_enroll
