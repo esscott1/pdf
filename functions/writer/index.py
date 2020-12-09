d027bb46ffa2b26e027f4530e5ec7b9f88315dd0
Eric Scott <escott@archer.local>
2020-12-09 12:55:01 -0700

    bug fix added writing to ca_packet table in DB

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -438,7 +438,7 @@ def write_dict_to_db(mydict, connection):
     fieldtextlist = []
     fieldvaluelist =  list(mydict.values())
     for fieldvalue in fieldvaluelist:
-        fieldtextlist.append(fieldvalue)
+        fieldtextlist.append(str(fieldvalue))
 
     print(sql, fieldvaluelist)
     print(fieldtextlist)
