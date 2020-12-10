ebc98aa98006cf5bd98691c9f0ee889a82d2d336
Eric Scott <escott@archer.local>
2020-12-10 10:01:49 -0700

    remove table as feature type

--- functions/reader/index.py
+++ functions/reader/index.py
@@ -20,7 +20,7 @@ def lambda_handler(event, context):
         },
         JobTag=key + '_Job',
         FeatureTypes=[
-            'FORMS','TABLES'
+            'FORMS'
         ],
         NotificationChannel={
             'RoleArn': os.environ['SNSROLEARN'],
