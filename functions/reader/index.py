aff61ba28e857befc29f81d95262748e0d1681c6
Eric Scott <escott@archer.local>
2020-12-21 19:01:32 -0700

    update readme for reading and printin excel

--- functions/reader/index.py
+++ functions/reader/index.py
@@ -2,6 +2,8 @@ import json
 import os
 import boto3
 import csv
+import pylightxl as xl
+import io
 
 def lambda_handler(event, context):
     print("Triggered getTextFromS3PDF event: " + json.dumps(event, indent=2))
@@ -11,29 +13,36 @@ def lambda_handler(event, context):
     key = event['Records'][0]['s3']['object']['key']
     if 'pdf' in str(key):
         print('---- found a pdf file ---')
+        try:
+            textract = boto3.client('textract')
+            textract.start_document_analysis(
+            DocumentLocation={
+                'S3Object': {
+                    'Bucket': bucket,
+                    'Name': key
+                }
+            },
+            JobTag=key + '_Job',
+            FeatureTypes=[
+                'FORMS'
+            ],
+            NotificationChannel={
+                'RoleArn': os.environ['SNSROLEARN'],
+                'SNSTopicArn': os.environ['SNSTOPIC']
+            })
+            
+            return 'Triggered PDF Processing for ' + key
+        except Exception as e:
+            print(e)
+            print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+            raise e
     else:
         print('--- did not find file ---')
-
-    try:
-        textract = boto3.client('textract')
-        textract.start_document_analysis(
-        DocumentLocation={
-            'S3Object': {
-                'Bucket': bucket,
-                'Name': key
-            }
-        },
-        JobTag=key + '_Job',
-        FeatureTypes=[
-            'FORMS'
-        ],
-        NotificationChannel={
-            'RoleArn': os.environ['SNSROLEARN'],
-            'SNSTopicArn': os.environ['SNSTOPIC']
-        })
-        
-        return 'Triggered PDF Processing for ' + key
-    except Exception as e:
-        print(e)
-        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-        raise e
+    if 'xlsx' in str(key):
+        print('---- found an Excel file ---')
+        s3 = boto3.client('s3')
+        s3.download_file(bucket,key,'test.xlsx')
+        wb = load_workbook.readxl(fn='test.xlsx')
+        print(wb.ws_names)
+    else:
+        print('--- did not find an Excel file ---')
--- testexcelreader.py
+++ testexcelreader.py
@@ -1,16 +1,20 @@
 import csv
 import pandas as pd
-from openpyxl import load_workbook
+import pylightxl as load_workbook
+import io
 
-wb = load_workbook(filename='c:/SampleData/pandatest.xlsx')
 
-print( wb.sheetnames)
-sheet_ranges =  wb['Sheet1']
 
-print(sheet_ranges['A1'].value)
-for sheet in wb:
-    print(f'----printing first row of sheet {sheet.title}all sheets ----')
-    for row in sheet.iter_rows(min_row=1,max_row=2):
-        for cell in row:
-            print(cell.value)
+
+wb = load_workbook.readxl(fn='c:/SampleData/pandatest.xlsx')
+
+print(wb.ws_names)
+#sheet_ranges =  wb['Sheet1']
+
+#print(sheet_ranges['A1'].value)
+#for sheet in wb:
+#    print(f'----printing first row of sheet {sheet.title}all sheets ----')
+#    for row in sheet.iter_rows(min_row=1,max_row=2):
+#        for cell in row:
+#            print(cell.value)
 
