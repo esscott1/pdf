7fa6eceb6dd00d3762cb851651bb6c068f2d41e7
Eric Scott <escott@archer.local>
2021-01-13 15:55:10 -0700

    bug fix Afft documents

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -98,9 +98,9 @@ def get_connection():
         return None
 
 def read_config():
-    global db_csv_2_ocr_map_enroll = {}
-    global db_csv_2_ocr_map_afft = {}
-    global csv_2_ocr_map_relfull = {}
+    global db_csv_2_ocr_map_enroll
+    global db_csv_2_ocr_map_afft
+    global csv_2_ocr_map_relfull
     bucket = 'archer-ocr-doc-bucket'
     s3 = boto3.client('s3')
     key = 'ocr_config.json'
