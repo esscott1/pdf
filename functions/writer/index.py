65db1d4b6bcfd70541e936926524b1adb607b8ff
Eric Scott <escott@archer.local>
2021-01-13 17:38:15 -0700

    refactor to methods

--- functions/writer/index.py
+++ functions/writer/index.py
@@ -237,7 +237,6 @@ def lambda_handler(event, context):
         pageno = pageno + 1
         print('---- page ',str(pageno),' ----',)
 
-        pagelines = page.lines
         lineNo = -1
         for line in page.lines:
             lineNo += 1
@@ -248,13 +247,9 @@ def lambda_handler(event, context):
 #                    ssn = str(word)
 #                    ca_ssn = word.confidence
 
-#        print(type(pagelines))
-#        print(len(pagelines))
-#        print('--- printing page lines ---')
-#        print(pagelines)
-#        print('--- did it print a page line number?')
+
         for csv_key in csv_2_ocr_map:    # Getting the keys to build up a row
-            if(str(csv_2_ocr_map[csv_key]['PageNo'] = str(pageno)
+            if(str(csv_2_ocr_map[csv_key]['PageNo'] = str(pageno):
                 print('Looking for csv_key is: ',csv_key,' | ocr key: ', csv_2_ocr_map[csv_key]['ocr_key'],' | at TopPos: ', str(csv_2_ocr_map[csv_key]['TopPos']),' on Page: ',str(pageno))
 
             #es = filter(lambda x: str(x.key).startswith(str(csv_2_ocr_map[csv_key]['ocr_key'])) and  csv_2_ocr_map[csv_key]['PageNo'] == pageno ,page.form.fields) 
